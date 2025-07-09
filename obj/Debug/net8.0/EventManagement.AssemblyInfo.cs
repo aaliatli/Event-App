25e461914589b65db4392a0c276fa6e1f93e18b2
Ali Atlı <aliatli2904@gmail.com>
2025-07-09 16:39:16 +0300

    Kodlar feature/add-searchtext branchine taşındı

--- obj/Debug/net8.0/EventManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56758501ddf06085a87e5043e8abc747c9b991d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cef5782759c4b6fa25fa003b814bf6c2cc78f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
