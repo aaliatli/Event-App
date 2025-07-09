eb85e5a73e53c49e527679c387442a55e8ff645b
Ali Atlı <aliatli2904@gmail.com>
2025-07-09 16:39:29 +0300

    Kodlar feature/add-pagination branchine taşındı

--- obj/Debug/net8.0/EventManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/EventManagement.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eed298bbcc657e90935df3c23946fa0a99f952c")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+872c37c55bb7b37f139809503b828d440ac9df0d")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
